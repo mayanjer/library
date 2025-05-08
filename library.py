a0b9b3b9700225bbb91805fac736cc6f521c86eb
mayanjer <mayanjer3@gmail.com>
2025-05-08 11:44:42 +0300

    removed the pre_existing errors and added some inheritance to the classes

--- library.py
+++ library.py
@@ -1,6 +1,9 @@
+class User:
+    def __init__(self, name):
+        self.name = name
 
 class Book: #book class that creates the books in the library
-    def __init__(self, title, author, number_of_copies = 0):
+    def __init__(self, title, author, number_of_copies = 1):
         self.title = title
         self.author = author
         self.number_of_copies = number_of_copies
@@ -8,22 +11,24 @@ class Book: #book class that creates the books in the library
 
 class Library: #creates relationships btn books and students who borrow them
     def __init__(self):
-        self.books = []
-        self.borrowers = []
+        self.__books = []
+        self.__borrowers = []
     
     def add_book(self, book):
-        self.books.append(book)
+        self.__books.append(book)
     
     def get_books(self):
-        return self.books
+        return self.__books
 
     def search_for_book(self, title):
-        for book in self.books:
+        for book in self.__books:
             if book.title == title:
-                
                 return book
-            else:
-                return f"Book with title {title} doesn't exist"
+        
+    def get_borrowers(self):
+        return self.__borrowers
+            
+        return f"Book with title {title} doesn't exist--You might want to check the spelling and try again"
     
     def borrow_book(self, title, student):
 
@@ -32,27 +37,28 @@ class Library: #creates relationships btn books and students who borrow them
         if isinstance (book, str):
             return book #for this case, the method, self.search_for_book returned an error message
 
-        if self.borrowers != []: #checking whether a student already has a book he hasnt returned b4 we give them a new one
-            for borrower in self.borrowers:
+        if self.__borrowers != []: #checking whether a student already has a book he hasnt returned b4 we give them a new one
+            for borrower in self.__borrowers:
                 if student.name == borrower.get("student").name:
                     return f"{borrower.get('student').name} already has a book, {borrower.get('book').title}"
 
         if book.number_of_copies > 0:
             
-            self.borrowers.append({"student":student,"book":book})
+            self.__borrowers.append({"student":student,"book":book})
 
-            if self.number_of_copies == 0:
-                self.books.remove(book)
+            if book.number_of_copies == 0:
+                self.__books.remove(book)
             return "Please take your book"
         else:
             return f"{book.title} is out of stock"
 
-
-
-class Student: #creates student objects that borrow the books
-    def __init__(self, name):
-        self.name = name
-    
+class Student(User): #creates student objects that borrow the book
+    def __init__(self, name, student_class):
+        super().__init__(name)
+        self.student_class = student_class
+        
     def get_student_details(self):
-        return self.name
+        return f"Name:\t{self.name}\nClass:\t{self.student_class}"
+    
+
 
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 
-from library import Book, Library, Student
+from library import Book, Library, Student, User
 
 lib = Library()
 
 bk_1 = Book("Introduction to python", "Mayanja")
-bk_2 = Book("Introduction to computer systems", "Muhammad", 3)
+bk_2 = Book("Introduction to computer systems", "Muhammad")
 
-sdt_1 = Student("Jeema")
-sdt_2 = Student("Kibirige")
+sdt_1 = Student("Jeema", "P2")
+sdt_2 = Student("Kibirige", "P5")
 
 lib.add_book(bk_1)
 lib.add_book(bk_2)
 
+print(lib.get_books())
+
 print(lib.borrow_book("Introduction to python", sdt_2))
-print(lib.borrow_book("Introduction to computer systems", sdt_2))
+print(lib.borrow_book("Introduction to computer systems", sdt_1))
+
+print(sdt_2.get_student_details())
